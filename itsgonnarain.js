f96583b8f59522aca2a4996d031bdf02180540d5
Devin Lane <devin.c.lane@gmail.com>
2022-12-19 17:43:57 -0800

    set up panning infrastructure

--- itsgonnarain.js
+++ itsgonnarain.js
@@ -3,25 +3,39 @@
 // create an audio context https://developer.mozilla.org/en-US/docs/Web/API/AudioContext
 const audioCtx = new AudioContext();
 
-function startLoop(audioBuffer) {
+function startLoop(audioBuffer, pan = 0) {
         // buffer source knows how to play an AudioBuffer
         // this AudioBufferSourceNode reacts in in the AudioBuffer data and streams it to other notes.
-        let sourceNode = audioCtx.createBufferSource();
+        const sourceNode = audioCtx.createBufferSource();
+
+        // set up panning
+        const pannerNode = audioCtx.createStereoPanner();
+
         // give the buffersource the audio buffer we have
         sourceNode.buffer = audioBuffer;
+
         // let's loop the audio
         sourceNode.loop = true;
+
         // set our loop start and stop points;
         const loopStart = 2.22
         const loopEnd = 2.9
         sourceNode.loopStart = loopStart;
         sourceNode.loopEnd = loopEnd;
-        // connect the buffer source to our audio context's destination (the speakers)
-        sourceNode.connect(audioCtx.destination);
+
+        // set the panning to pan value
+        pannerNode.pan.value = pan;
+
+        // connect the source node to the panner node
+        sourceNode.connect(pannerNode);
+        // connect the panner node to the buffer source to our audio context's destination (the speakers)
+        pannerNode.connect(audioCtx.destination)
+
         // start the music, only play between our loops
         // first argument is when to start playing, we set to 0 to say immediately
         // second argument is the offset of how far in to our song to start playing, we set to the loop beginning
         sourceNode.start(0, loopStart)
+
         // grab the stop button
         const stop = document.getElementById('stop')
         // add event listener
